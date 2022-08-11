789fed4e148c025e49d1e5ec2fde3f7cbcb7399d
Ekaterina Kulazhenkova <kulazhenkova.k@gmail.com>
2022-08-11 23:19:17 +0300

    HW task 2 end

---
+++ HW_2/HW2.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите a =");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите b =");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+
+if (a > b) max = a;
+
+else max = b;
+
+Console.Write("max=");
+Console.WriteLine(max);
