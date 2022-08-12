79a2f553b4547ad1c256a6472bf5ceaf9f0d9be7
Ekaterina Kulazhenkova <kulazhenkova.k@gmail.com>
2022-08-12 08:07:50 +0300

    Не решенная 8 задача

--- HW_8/HW8.cs
+++ HW_8/HW8.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите значение N");
+int N = Convert.ToInt32(Console.ReadLine());
+int a = 0;
+
+while (true)
+    if (a > N)
+    {
+        break
+    }
+if (a % 2 == 0)
+{
+    Console.WriteLine("четные числа");
+    Console.Write(a);
+}
+int a = (a + 1);
